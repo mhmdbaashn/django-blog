892b8ee8cffecad48482f8a279156b1eb1d2bcff
MOHAMMED BAASHN <ir.mnmn.maob@gmail.com>
2024-11-08 19:10:09 +0000

    deployed

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,8 +19,8 @@ from django.urls import path
 from blog.views import my_blog
 
 urlpatterns = [
+    path("blog/", blog_views.my_blog, name='blog'),
     path('admin/', admin.site.urls),
-    path('', my_blog, name='blog'),
 
 
 ]
