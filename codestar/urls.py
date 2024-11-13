333550263bdea37d6f93b85854c5a2908b14fed4
Mohammed Baashn <ir.mnmn.maob@gmail.com>
2024-11-13 11:34:36 +0000

    readies code for deploy

--- codestar/urls.py
+++ codestar/urls.py
@@ -22,5 +22,7 @@ from blog.views import my_blog
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', my_blog, name='blog'),
+    path('', index_views.index, name='index'),
+
 
 ]
