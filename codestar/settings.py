ecc78e670dc7ab18e40df971c9e30c5135fa0648
Mohammed Baashn <ir.mnmn.maob@gmail.com>
2024-11-13 12:01:58 +0000

    Add Secret key to env file

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ='django-insecure-%+z3b9$c6fk$o&1=my(hw)e6ze$hmb17*r!1fv$*j=lp0mov++'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-mhmdbaashn-djangoblog-vlurvm7zhq4.ws.codeinstitute-ide.net',
                 '.herokuapp.com'
