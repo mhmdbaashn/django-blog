e3eacbd839dc5fa6e176eb9d4ae27362485cd356
Mohammed Baashn <ir.mnmn.maob@gmail.com>
2024-11-18 10:33:06 +0000

    enable serving of image files

--- codestar/settings.py
+++ codestar/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
     'allauth.socialaccount',
     'django_summernote',
+    'cloudinary',
     'blog',
     'about',
 ]
